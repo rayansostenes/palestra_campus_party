660ecb8ce511045d4e9e83a733b53a773e82cdf0
Rayan Sóstenes <rayancocos@gmail.com>
2019-09-07 13:41:41 -0300

    Added 2 routes

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from framework import App
+from framework import App, Response
 
+app = App()
 
-application = App()
+@app.route(r'^/$')
+def index(request):
+    return Response('<h1>Hello Campus Party</h1>')
+
+@app.route(r'^/hello/(\w+)$')
+def hello(request, name):
+    return Response(f'<h1>Hello, {name} </h1>')
+
+application = app
